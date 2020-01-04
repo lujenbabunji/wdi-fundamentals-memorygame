8fdaed4eb48061acaa17379f194f39e03280f579
lujenbabunji <lujen.babunji@gmail.com>
2020-01-04 15:52:34 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,31 +1,34 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
 const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  
-    alert("You found a match!");
-  }
-  else {
-
-	alert("Sorry, try again.");
-  }
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+     console.log("You found a match!");
+     }
+     else {
+     console.log("Sorry, try again.");
+     }
 }
+
+
+
+function flipCard(cardId){
+   
+     console.log("User flipped " + cards[cardId]);
+     cardsInPlay.push(cards[cardId]);
+
+     if (cardsInPlay.length === 2) {
+         checkForMatch(); 
+     }
+}
+
+
+flipCard(0);
+flipCard(2);
+
+ 
+
  
